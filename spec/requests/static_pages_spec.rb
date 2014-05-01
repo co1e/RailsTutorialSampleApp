d14fa1d92d4686939941d22cd4ead0decc977208
Thomas Cole <thomas@co1e.com>
2014-04-30 21:58:09 -0400

    Added base_title to tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   describe "Home page" do
   
@@ -11,7 +12,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   
@@ -23,7 +24,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   
@@ -36,7 +37,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
   
@@ -49,7 +50,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title('Contact Us')
+      expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
 end
